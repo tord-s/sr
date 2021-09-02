7325c926e431fd3c3695954db62b49ba1e7eef26
tord-s <tord@softeland.name>
2021-09-02 19:15:59 +0200

    tweeks

--- src/index.js
+++ src/index.js
@@ -13,14 +13,15 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    frame: false 
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
